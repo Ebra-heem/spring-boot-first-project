2fd777b782fd6bdce0d2b247fe37eb3378eb0dd9
Ebra-heem <im.ibrahim420@gmail.com>
2024-06-01 17:30:49 +0600

    Implementing DAO Layer using Repository Pattern

--- src/main/java/com/ebrahim/springdemo/student/StudentService.java
+++ src/main/java/com/ebrahim/springdemo/student/StudentService.java
@@ -1,30 +1,19 @@
 package com.ebrahim.springdemo.student;
 
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
 
-public class StudentService {
+public interface StudentService {
 
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student(
-                        "Ebrahim",
-                        "Kholil",
-                        LocalDate.now(),
-                        "admin@gmail.com",
-                        29
-                ),
-                new Student(
-                        "Ishmael Xaviul",
-                        "Idrak",
-                        LocalDate.now(),
-                        "Idrakn@gmail.com",
-                        2
-                )
-        );
-    }
+   Student save(Student s);
+
+   List<Student> findAllStudents();
+
+   Student findByEmail(String email);
+
+   Student update(Student s);
+
+   void delete(String email);
 }
